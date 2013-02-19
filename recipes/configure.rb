ae16761c78adb5ee8d713ce9d35ee84c9a1b020b
Jim Neath <jimneath@googlemail.com>
2013-02-19 10:32:03 +0000

    Fixed old sidekiq recipe so it actually works

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,39 +1,37 @@
 if sidekiq_instance?
-
   # for now
   worker_count = 1
+  
+  template "/engineyard/bin/sidekiq" do
+    owner 'root'
+    group 'root' 
+    mode 0755
+    source "sidekiq.erb" 
+  end
 
-  node[:applications].each do |app, data|
-    template "/etc/monit.d/sidekiq_#{app}.monitrc" do 
+  node[:applications].each do |app_name, data|
+    template "/etc/monit.d/sidekiq_#{app_name}.monitrc" do 
       owner 'root' 
       group 'root' 
       mode 0644 
       source "monitrc.conf.erb" 
       variables({ 
         :num_workers => worker_count,
-        :app_name => app, 
+        :app_name => app_name, 
         :rails_env => node[:environment][:framework_env] 
       }) 
     end
 
-    template "/engineyard/bin/sidekiq" do
-      owner 'root'
-      group 'root' 
-      mode 0755
-      source "sidekiq.erb" 
-    end
-
     worker_count.times do |count|
-      template "/data/#{app}/shared/config/sidekiq_#{count}.yml" do
+      template "/data/#{app_name}/shared/config/sidekiq_#{count}.yml" do
         owner node[:owner_name]
         group node[:owner_name]
         mode 0644
         source "sidekiq.yml.erb"
         variables({
-          :require => "/data/#{app}/current"
+          :require => "/data/#{app_name}/current"
         })
       end
     end
-
   end 
 end
--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,16 +1,13 @@
-
 if sidekiq_instance?
-
   execute "ensure-sidekiq-is-setup-with-monit" do 
-    command %Q{ 
-      monit reload 
-    } 
+    command "monit reload"
   end
 
-  execute "restart-sidekiq" do 
-    command %Q{ 
-      echo "sleep 20 && monit -g #{app}_sidekiq restart all" | at now 
-    }
+  node[:applications].each do |app_name, data|
+    execute "restart-sidekiq" do 
+      command %Q{ 
+        echo "sleep 20 && monit -g #{app_name}_sidekiq restart all" | at now 
+      }
+    end
   end
-
 end
